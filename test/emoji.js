bce7da4beb671a70e1fa8335761f0508710c4397
Roopak Venkatakrishnan <roopak.v@gmail.com>
2019-02-15 12:55:04 -0800

    Add more emoji - v4.0.4 of js-emoji (#72)
    * add more emoji
    
    oops
    
    update emojiparse too
    
    * fix tests

--- test/emoji.js
+++ test/emoji.js
@@ -11,13 +11,13 @@ describe("emoji.js", function () {
     it("should return an emoji code", function () {
       var coffee = emoji.get('coffee');
       should.exist(coffee);
-      coffee.should.be.exactly('☕️');
+      coffee.should.be.exactly('☕');
     });
 
     it("should support github flavored markdown emoji", function () {
       var coffee = emoji.get(':coffee:');
       should.exist(coffee);
-      coffee.should.be.exactly('☕️');
+      coffee.should.be.exactly('☕');
     });
   });
 
@@ -33,7 +33,7 @@ describe("emoji.js", function () {
 
   describe("which(emoji_code)", function () {
     it("should return name of the emoji", function () {
-      var coffee = emoji.which('☕️');
+      var coffee = emoji.which('☕');
       should.exist(coffee);
       coffee.should.be.exactly('coffee');
     });
@@ -41,7 +41,7 @@ describe("emoji.js", function () {
     it("should work for differently formed characters", function () {
       var umbrella = emoji.which('☔');
       should.exist(umbrella);
-      umbrella.should.be.exactly('umbrella');
+      umbrella.should.be.exactly('umbrella_with_rain_drops');
     });
 
     it("should return the same name for differently formed characters", function () {
@@ -60,7 +60,7 @@ describe("emoji.js", function () {
       var marocco = emoji.which('🇲🇦');
       should.exists(marocco);
       marocco.should.be.exactly('flag-ma');
-      
+
       // see issue #21
       mexico.should.not.equal(marocco);
     });
@@ -70,7 +70,7 @@ describe("emoji.js", function () {
     it("should parse :emoji: in a string and replace them with the right emoji", function () {
       var coffee = emoji.emojify('I :heart:  :coffee:! -  :hushed::star::heart_eyes:  ::: test : : :+1:+');
       should.exist(coffee);
-      coffee.should.be.exactly('I ❤️  ☕️! -  😯⭐️😍  ::: test : : 👍+');
+      coffee.should.be.exactly('I ❤️  ☕! -  😯⭐😍  ::: test : : 👍+');
     });
 
     it("should handle flags correctly", function() {
@@ -82,7 +82,7 @@ describe("emoji.js", function () {
     it("should leave unknown emoji", function () {
       var coffee = emoji.emojify('I :unknown_emoji: :star: :another_one:');
       should.exist(coffee);
-      coffee.should.be.exactly('I :unknown_emoji: ⭐️ :another_one:');
+      coffee.should.be.exactly('I :unknown_emoji: ⭐ :another_one:');
     });
 
     it("should replace unknown emoji using provided cb function", function () {
@@ -90,7 +90,7 @@ describe("emoji.js", function () {
         return name;
       });
       should.exist(coffee);
-      coffee.should.be.exactly('I unknown_emoji ⭐️ another_one');
+      coffee.should.be.exactly('I unknown_emoji ⭐ another_one');
     });
 
     it("should wrap emoji using provided format function", function () {
@@ -99,7 +99,7 @@ describe("emoji.js", function () {
       });
 
       should.exist(coffee);
-      coffee.should.be.exactly('I <img alt="❤️" src="heart.png" /> <img alt="☕️" src="coffee.png" />');
+      coffee.should.be.exactly('I <img alt="❤️" src="heart.png" /> <img alt="☕" src="coffee.png" />');
     });
 
     it("should not wrap unknown using provided format function", function () {
@@ -108,28 +108,28 @@ describe("emoji.js", function () {
       });
 
       should.exist(coffee);
-      coffee.should.be.exactly('I :unknown_emoji: <img alt="☕️" src="coffee.png" />');
+      coffee.should.be.exactly('I :unknown_emoji: <img alt="☕" src="coffee.png" />');
     });
 
     it("should replace unknown emojis and wrap known emojis using cb functions", function () {
-      var coffee = emoji.emojify('I :unknown_emoji: :coffee:', 
+      var coffee = emoji.emojify('I :unknown_emoji: :coffee:',
         function(name) {
           return name;
-        }, 
+        },
         function(code, name) {
           return '<img alt="' + code + '" src="' + name + '.png" />';
         }
       );
 
       should.exist(coffee);
-      coffee.should.be.exactly('I unknown_emoji <img alt="☕️" src="coffee.png" />');
+      coffee.should.be.exactly('I unknown_emoji <img alt="☕" src="coffee.png" />');
     });
   });
 
   it("should return an emoji code", function () {
     var coffee = emoji.emoji.coffee;
     should.exist(coffee);
-    coffee.should.be.exactly('☕️');
+    coffee.should.be.exactly('☕');
   });
 
   describe("search(str)", function () {
@@ -280,7 +280,7 @@ describe("emoji.js", function () {
     });
 
     it('Should be able to replace by callback function', function() {
-      var result = emoji.replace('There is no ⚠ on my hard drive', function (emoji) { 
+      var result = emoji.replace('There is no ⚠ on my hard drive', function (emoji) {
         return emoji.key;
       });
       result.should.equal('There is no warning on my hard drive');
