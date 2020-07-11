bd37c5554fdee4a5e1638d28d64288fb4db59fa8
Richie Bendall <richiebendall@gmail.com>
2020-07-11 13:58:12 +1200

    Export functions via `exports`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function replace (string, replacement, { removeSpaces = false } = {}) {
 
   return chars
     .map((char, i) => {
-      const emoji = core.which(char)
+      const emoji = exports.which(char)
 
       if (!emoji) {
         return char
@@ -45,94 +45,90 @@ function replace (string, replacement, { removeSpaces = false } = {}) {
     .join('')
 }
 
-const core = {
-  get (key) {
-    ow(key, ow.string)
+exports.get = key => {
+  ow(key, ow.string)
 
-    return emoji.get(normalizeName(key))
-  },
-
-  which (emoji, { markdown = false } = {}) {
-    ow(emoji, ow.string)
-    ow(markdown, ow.boolean)
-
-    const res = inverted.get(skinTone(emoji, 'none'))
-
-    if (res === undefined) {
-      return undefined
-    }
-
-    return markdown ? `:${res}:` : res
-  },
-
-  random () {
-    const [key, emoji] = emojiData[Math.floor(Math.random() * emojiData.length)]
-    return { key, emoji }
-  },
-
-  search (key) {
-    ow(key, ow.string)
-
-    key = normalizeName(key)
-
-    return emojiData
-      .filter(([name]) => name.includes(key))
-      .map(([key, emoji]) => ({ key, emoji }))
-  },
-
-  find (emoji) {
-    ow(emoji, ow.string)
-
-    const key = core.which(emoji)
-
-    if (key === undefined) {
-      return undefined
-    }
-
-    return { key, emoji }
-  },
-
-  has (key) {
-    ow(key, ow.string)
-
-    return emoji.has(normalizeName(key)) || inverted.has(key)
-  },
-
-  replace (string, replacement) {
-    return replace(string, replacement)
-  },
-
-  strip (string, { removeSpaces = true } = {}) {
-    return replace(string, '', { removeSpaces })
-  },
-
-  emojify (string, { fallback = '', format = (value) => value } = {}) {
-    ow(string, ow.string)
-
-    return string
-      .split(/:([a-zA-Z0-9_\-+ ]+):/g)
-      .map((str, i) => {
-        if (i % 2 === 0) {
-          return str
-        }
-
-        const emoji = core.get(str)
-        return emoji ? format(emoji, str, string) : fallback
-      })
-      .join('')
-  },
-
-  unemojify (string) {
-    ow(string, ow.string)
-
-    return string.match(charRegex)
-      .map(char => core.which(char, { markdown: true }) || char)
-      .join('')
-  },
-
-  findAll (string) {
-    return core.emojify(string).match(emojiRegex).map(char => core.find(char))
-  }
+  return emoji.get(normalizeName(key))
 }
 
-module.exports = core
+exports.which = (emoji, { markdown = false } = {}) => {
+  ow(emoji, ow.string)
+  ow(markdown, ow.boolean)
+
+  const res = inverted.get(skinTone(emoji, 'none'))
+
+  if (res === undefined) {
+    return undefined
+  }
+
+  return markdown ? `:${res}:` : res
+}
+
+exports.random = () => {
+  const [key, emoji] = emojiData[Math.floor(Math.random() * emojiData.length)]
+  return { key, emoji }
+}
+
+exports.search = key => {
+  ow(key, ow.string)
+
+  key = normalizeName(key)
+
+  return emojiData
+    .filter(([name]) => name.includes(key))
+    .map(([key, emoji]) => ({ key, emoji }))
+}
+
+exports.find = emoji => {
+  ow(emoji, ow.string)
+
+  const key = exports.which(emoji)
+
+  if (key === undefined) {
+    return undefined
+  }
+
+  return { key, emoji }
+}
+
+exports.has = key => {
+  ow(key, ow.string)
+
+  return emoji.has(normalizeName(key)) || inverted.has(key)
+}
+
+exports.replace = (string, replacement) => {
+  return replace(string, replacement)
+}
+
+exports.strip = (string, { removeSpaces = true } = {}) => {
+  return replace(string, '', { removeSpaces })
+}
+
+exports.emojify = (string, { fallback = '', format = (value) => value } = {}) => {
+  ow(string, ow.string)
+
+  return string
+    .split(/:([a-zA-Z0-9_\-+ ]+):/g)
+    .map((str, i) => {
+      if (i % 2 === 0) {
+        return str
+      }
+
+      const emoji = exports.get(str)
+      return emoji ? format(emoji, str, string) : fallback
+    })
+    .join('')
+}
+
+exports.unemojify = (string) => {
+  ow(string, ow.string)
+
+  return string.match(charRegex)
+    .map(char => exports.which(char, { markdown: true }) || char)
+    .join('')
+}
+
+exports.findAll = (string) => {
+  return exports.emojify(string).match(emojiRegex).map(char => exports.find(char))
+}
