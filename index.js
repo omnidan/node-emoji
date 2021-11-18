8b65708c1bb389ef74cc00e4a4a2d981b0f45981
Josh Goldberg <me@joshuakgoldberg.com>
2021-11-18 15:48:05 -0700

    Added Prettier to better match existing settings

--- index.js
+++ index.js
@@ -1,155 +1,153 @@
-"use strict";
+'use strict'
 
-const is = require("@sindresorhus/is");
-const charRegex = require("char-regex")();
-const emojilib = require("emojilib");
-const emojiRegex = require("emoji-regex")();
-const skinTone = require("skin-tone");
+const is = require('@sindresorhus/is')
+const charRegex = require('char-regex')()
+const emojilib = require('emojilib')
+const emojiRegex = require('emoji-regex')()
+const skinTone = require('skin-tone')
 
-const { assert } = is;
+const { assert } = is
 
 function randomItem(array) {
-  return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)]
 }
 
 const emojiData = Object.entries(emojilib.lib).map(
   ([name, { char: emoji }]) => [name, emoji]
-);
+)
 
-const emojiEntries = new Map(emojiData);
+const emojiEntries = new Map(emojiData)
 const emojiEntriesInverted = new Map(
   emojiData.map(([name, emoji]) => [emoji, name])
-);
+)
 
 function normalizeName(name) {
   if (/:.+:/.test(name)) {
-    name = name.slice(1, -1);
+    name = name.slice(1, -1)
   }
 
-  return name;
+  return name
 }
 
 function replace(input, replacement, { preserveSpaces = false } = {}) {
-  assert.string(input);
-  assert.any([is.string, is.function], replacement);
-  assert.boolean(preserveSpaces);
+  assert.string(input)
+  assert.any([is.string, is.function], replacement)
+  assert.boolean(preserveSpaces)
 
   const replaceFunction =
-    typeof replacement === "function" ? replacement : () => replacement;
+    typeof replacement === 'function' ? replacement : () => replacement
 
-  const characters = input.match(charRegex);
+  const characters = input.match(charRegex)
 
   if (characters === null) {
-    return input;
+    return input
   }
 
   return characters
     .map((character, index) => {
-      const emoji = exports.which(character);
+      const emoji = exports.which(character)
 
       if (!emoji) {
-        return character;
+        return character
       }
 
-      if (!preserveSpaces && characters[index + 1] === " ") {
-        characters[index + 1] = "";
+      if (!preserveSpaces && characters[index + 1] === ' ') {
+        characters[index + 1] = ''
       }
 
-      return replaceFunction(emoji, index, input);
+      return replaceFunction(emoji, index, input)
     })
-    .join("");
+    .join('')
 }
 
 exports.get = (name) => {
-  assert.string(name);
+  assert.string(name)
 
-  return emojiEntries.get(normalizeName(name));
-};
+  return emojiEntries.get(normalizeName(name))
+}
 
 exports.which = (emoji, { markdown = false } = {}) => {
-  assert.string(emoji);
-  assert.boolean(markdown);
+  assert.string(emoji)
+  assert.boolean(markdown)
 
-  const result = emojiEntriesInverted.get(skinTone(emoji, "none"));
+  const result = emojiEntriesInverted.get(skinTone(emoji, 'none'))
 
   if (result === undefined) {
-    return undefined;
+    return undefined
   }
 
-  return markdown ? `:${result}:` : result;
-};
+  return markdown ? `:${result}:` : result
+}
 
 exports.random = () => {
-  const [name, emoji] = randomItem(emojiData);
-  return { name, emoji };
-};
+  const [name, emoji] = randomItem(emojiData)
+  return { name, emoji }
+}
 
 exports.search = (keyword) => {
-  assert.string(keyword);
+  assert.string(keyword)
 
-  keyword = normalizeName(keyword);
+  keyword = normalizeName(keyword)
 
   return emojiData
     .filter(([name]) => name.includes(keyword))
-    .map(([name, emoji]) => ({ name, emoji }));
-};
+    .map(([name, emoji]) => ({ name, emoji }))
+}
 
 exports.find = (emoji) => {
-  assert.string(emoji);
+  assert.string(emoji)
 
-  const name = exports.which(emoji);
+  const name = exports.which(emoji)
 
   if (name === undefined) {
-    return undefined;
+    return undefined
   }
 
-  return { name, emoji };
-};
+  return { name, emoji }
+}
 
 exports.has = (name) => {
-  assert.string(name);
+  assert.string(name)
 
-  return (
-    emojiEntries.has(normalizeName(name)) || emojiEntriesInverted.has(name)
-  );
-};
+  return emojiEntries.has(normalizeName(name)) || emojiEntriesInverted.has(name)
+}
 
-exports.replace = replace;
+exports.replace = replace
 
 exports.strip = (input, { preserveSpaces } = {}) =>
-  replace(input, "", { preserveSpaces });
+  replace(input, '', { preserveSpaces })
 
 exports.emojify = (
   input,
-  { fallback = "", format = (value) => value } = {}
+  { fallback = '', format = (value) => value } = {}
 ) => {
-  assert.string(input);
+  assert.string(input)
 
   return input
     .split(/:([a-zA-Z0-9_\-+ ]+):/g)
     .map((part, index) => {
       if (index % 2 === 0) {
-        return part;
+        return part
       }
 
-      const emoji = exports.get(part);
-      return emoji ? format(emoji, part, input) : fallback;
+      const emoji = exports.get(part)
+      return emoji ? format(emoji, part, input) : fallback
     })
-    .join("");
-};
+    .join('')
+}
 
 exports.unemojify = (input) => {
-  assert.string(input);
+  assert.string(input)
 
   return input
     .match(charRegex)
     .map(
       (character) => exports.which(character, { markdown: true }) || character
     )
-    .join("");
-};
+    .join('')
+}
 
 exports.findAll = (input) =>
   (exports.emojify(input).match(emojiRegex) || []).map((character) =>
     exports.find(character)
-  );
+  )
--- index.test.js
+++ index.test.js
@@ -1,92 +1,92 @@
-const { describe, expect, it } = require("@jest/globals");
-const emoji = require(".");
+const { describe, expect, it } = require('@jest/globals')
+const emoji = require('.')
 
-describe("node-emoji", () => {
-  it("get", () => {
-    expect(emoji.get("coffee")).toEqual("☕");
-    expect(emoji.get(":coffee:")).toEqual("☕");
-  });
+describe('node-emoji', () => {
+  test('get', () => {
+    expect(emoji.get('coffee')).toEqual('☕')
+    expect(emoji.get(':coffee:')).toEqual('☕')
+  })
 
-  it("which", () => {
-    expect(emoji.which("☕")).toEqual("coffee");
-    expect(emoji.which("☕", { markdown: true })).toEqual(":coffee:");
-    expect(emoji.which("👍🏾")).toEqual("+1");
-    expect(emoji.which("👍🏾", { markdown: true })).toEqual(":+1:");
-  });
+  test('which', () => {
+    expect(emoji.which('☕')).toEqual('coffee')
+    expect(emoji.which('☕', { markdown: true })).toEqual(':coffee:')
+    expect(emoji.which('👍🏾')).toEqual('+1')
+    expect(emoji.which('👍🏾', { markdown: true })).toEqual(':+1:')
+  })
 
-  it("has", () => {
-    expect(emoji.has("☕")).toBe(true);
-    expect(emoji.has("coffee")).toBe(true);
-    expect(emoji.has(":coffee:")).toBe(true);
-    expect(emoji.has("idontexist")).toBe(false);
-    expect(emoji.has(":idontexist:")).toBe(false);
-  });
+  test('has', () => {
+    expect(emoji.has('☕')).toBe(true)
+    expect(emoji.has('coffee')).toBe(true)
+    expect(emoji.has(':coffee:')).toBe(true)
+    expect(emoji.has('idontexist')).toBe(false)
+    expect(emoji.has(':idontexist:')).toBe(false)
+  })
 
-  it("random", () => {
-    const data = emoji.random();
-    expect(emoji.has(data.name)).toBe(true);
-    expect(emoji.has(data.emoji)).toBe(true);
-  });
+  test('random', () => {
+    const data = emoji.random()
+    expect(emoji.has(data.name)).toBe(true)
+    expect(emoji.has(data.emoji)).toBe(true)
+  })
 
-  it("replace", () => {
-    expect(emoji.replace("", "b")).toEqual("");
-    expect(emoji.replace("no emojis", "b")).toEqual("no emojis");
-    expect(emoji.replace("a ☕ c", "b")).toEqual("a bc");
-    expect(emoji.replace("a 🌭 c", () => "b")).toEqual("a bc");
+  test('replace', () => {
+    expect(emoji.replace('', 'b')).toEqual('')
+    expect(emoji.replace('no emojis', 'b')).toEqual('no emojis')
+    expect(emoji.replace('a ☕ c', 'b')).toEqual('a bc')
+    expect(emoji.replace('a 🌭 c', () => 'b')).toEqual('a bc')
     expect(
       emoji.replace(
-        "a 🌭 b 🌭 🌭🌭 c",
+        'a 🌭 b 🌭 🌭🌭 c',
         (() => {
-          let counter = 0;
-          const letters = ["w", "x", "y", "z"];
-          return () => letters[counter++];
+          let counter = 0
+          const letters = ['w', 'x', 'y', 'z']
+          return () => letters[counter++]
         })()
       )
-    ).toEqual("a wb xyzc");
-  });
+    ).toEqual('a wb xyzc')
+  })
 
-  it("strip", () => {
-    expect(emoji.strip("a ☕ c")).toEqual("a c");
-    expect(emoji.strip("a ☕ c", { preserveSpaces: false })).toEqual("a c");
-    expect(emoji.strip("a ☕ c", { preserveSpaces: true })).toEqual("a  c");
-  });
+  test('strip', () => {
+    expect(emoji.strip('a ☕ c')).toEqual('a c')
+    expect(emoji.strip('a ☕ c', { preserveSpaces: false })).toEqual('a c')
+    expect(emoji.strip('a ☕ c', { preserveSpaces: true })).toEqual('a  c')
+  })
 
-  it("emojify", () => {
-    expect(emoji.emojify("a :coffee: c")).toEqual("a ☕ c");
-    expect(emoji.emojify("a :coffee: c :idontexist: d")).toEqual("a ☕ c  d");
-  });
+  test('emojify', () => {
+    expect(emoji.emojify('a :coffee: c')).toEqual('a ☕ c')
+    expect(emoji.emojify('a :coffee: c :idontexist: d')).toEqual('a ☕ c  d')
+  })
 
-  it("unemojify", () => {
-    expect(emoji.unemojify("a ☕ c")).toEqual("a :coffee: c");
-    expect(emoji.unemojify("a ☕ 🌭 c")).toEqual("a :coffee: :hotdog: c");
-  });
+  test('unemojify', () => {
+    expect(emoji.unemojify('a ☕ c')).toEqual('a :coffee: c')
+    expect(emoji.unemojify('a ☕ 🌭 c')).toEqual('a :coffee: :hotdog: c')
+  })
 
-  it("search", () => {
-    expect(emoji.search("100")).toEqual([{ name: "100", emoji: "💯" }]);
-  });
+  test('search', () => {
+    expect(emoji.search('100')).toEqual([{ name: '100', emoji: '💯' }])
+  })
 
-  it("find", () => {
-    expect(emoji.find("💯")).toEqual({ name: "100", emoji: "💯" });
-    expect(emoji.find("a")).toEqual(undefined);
-  });
+  test('find', () => {
+    expect(emoji.find('💯')).toEqual({ name: '100', emoji: '💯' })
+    expect(emoji.find('a')).toEqual(undefined)
+  })
 
-  it("findAll", () => {
-    expect(emoji.findAll("")).toEqual([]);
-    expect(emoji.findAll("unrelated")).toEqual([]);
-    expect(emoji.findAll(":idontexist:")).toEqual([]);
-    expect(emoji.findAll("I :heart: ☕ and :pizza:!")).toEqual([
+  test('findAll', () => {
+    expect(emoji.findAll('')).toEqual([])
+    expect(emoji.findAll('unrelated')).toEqual([])
+    expect(emoji.findAll(':idontexist:')).toEqual([])
+    expect(emoji.findAll('I :heart: ☕ and :pizza:!')).toEqual([
       {
-        emoji: "❤️",
-        name: "heart",
+        emoji: '❤️',
+        name: 'heart',
       },
       {
-        emoji: "☕",
-        name: "coffee",
+        emoji: '☕',
+        name: 'coffee',
       },
       {
-        emoji: "🍕",
-        name: "pizza",
+        emoji: '🍕',
+        name: 'pizza',
       },
-    ]);
-  });
-});
+    ])
+  })
+})
