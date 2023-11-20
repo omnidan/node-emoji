b53ad876a43ee9ea3f003d7e2489847b0391f1c2
Josh Goldberg <git@joshuakgoldberg.com>
2023-11-20 08:42:19 +0100

    Move CJS test to its own file

--- src/e2e.cjs
+++ src/e2e.cjs
@@ -2,4 +2,4 @@ const { strict: assert } = require('node:assert')
 
 const emoji = require('../lib/index.cjs')
 
-assert.equal(emoji.emojify(':wave:'), '👋')
+assert.equal(emoji.emojify(':wave:'), 'intentional failure')
