7046cbbdda4282b9503ea10c7ac632f39bba74bd
Alexander Ikonomou <alexander.ikonomou.19@proton.me>
2024-10-01 00:11:10 +0200

    feat: allow RegExp objects to be passed to search that are evaluated for each emoji

--- src/search.ts
+++ src/search.ts
@@ -1,14 +1,21 @@
-import { assert } from '@sindresorhus/is'
+import is, { assert } from '@sindresorhus/is'
 
 import { emojiData } from './data.js'
 import { normalizeName } from './utils.js'
 
-export const search = (keyword: string) => {
-  assert.string(keyword)
+export const search = (keyword: RegExp | string) => {
+  assert.any([is.default.string, is.default.regExp], keyword)
 
-  keyword = normalizeName(keyword)
+  if (is.default.string(keyword)) {
+    keyword = normalizeName(keyword)
+  }
+
+  if (is.default.regExp(keyword)) {
+    const normalizedPattern = normalizeName(keyword.source)
+    keyword = new RegExp(normalizedPattern)
+  }
 
   return emojiData
-    .filter(([name]) => name.includes(keyword))
+    .filter(([name]) => name.match(keyword))
     .map(([name, emoji]) => ({ emoji, name }))
 }
